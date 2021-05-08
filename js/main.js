1084c0e454eb1bb126204841ef59f23c67c49e5d
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-08 23:23:09 +0700

    [Massive]: Implement Porfolio item and remove owl carousel

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Init Portfolio Popup
+$(".js-portfolio-popup").magnificPopup({
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
+
+$(".js-portfolio-popup2").magnificPopup({
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
